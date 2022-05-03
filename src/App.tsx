2f3c133700fb5fcffd937a04be4d9cdc13b6c387
jbiers <juliabiersuriano@gmail.com>
2022-05-03 00:06:37 -0400

    added tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { isPropertySignature } from 'typescript';
+
+interface ButtonProps {
+  text: string;
+}
+
+function Button({ text }: ButtonProps) {
+  return <button className="bg-violet-500 p-2">{text ?? 'Default'}</button>
+}
 
 function App() {
   return (
-    <h1>Hello DEV</h1>
+    <div>
+      <Button text="Enviar"/>
+      <Button text="OK"/>
+    </div>
   );
 }
 
