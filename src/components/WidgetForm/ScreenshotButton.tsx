55920c56ab17d8b74c09876632c8968430290f3d
jbiers <juliabiersuriano@gmail.com>
2022-05-10 11:19:34 -0400

    added loading animation

--- src/components/WidgetForm/ScreenshotButton.tsx
+++ src/components/WidgetForm/ScreenshotButton.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import html2canvas from "html2canvas";
 import { Camera } from "phosphor-react";
+import { Loading } from "../Loading";
 
 export function ScreenshotButton() {
     const [isTakingScreenshot, setIsTakingScreenshot ] = useState(false);
@@ -21,7 +22,7 @@ export function ScreenshotButton() {
           onClick={handleTakeScreenshot}
           className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
         >
-            <Camera className="w-6 h-6"/>
+            { isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6"/> }
         </button>
     )
 }
