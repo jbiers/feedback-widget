4a2abcf4aac24128b3bbed76407d9af42a270e0c
jbiers <juliabiersuriano@gmail.com>
2022-05-13 15:59:09 -0400

    added email sending service

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 ### tailwindcss
 
 ### express, nodemon, app.use(express.json())
+
+### Prisma. Very cool, but could you do the SQL queries yourself?
+
+### Sending emails, huh? SMTP Gmail. Mailtrap
+
+-SOLID
+
+- Single Responsibility Principle
+- Open/Closed Principle
+- Liskov Substitution Principle
+- Interface Segregation Principle
+- Dependendy Inversion Principle
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,45 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+
+import { prisma } from './prisma';
 
 const app = express();
 
 app.use(express.json())
 
-app.post('/feedbacks', (req, res) => {
-    const {type, comment} = req.body;
-    console.log(type);
-    console.log(comment);
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "f6a4269c069438",
+      pass: "40aca605410a60"
+    }
+});
 
-    return res.send("It worked!");
+app.post('/feedbacks', async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot,
+        }
+    })
+
+    await transport.sendMail({
+        from: 'FeedGet Team <oi@feedget.com>',
+        to: 'Julia <juliabiersuriano@gmail.com>',
+        subject: 'New feedback',
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Feedback type: ${type}</p>`,
+            `<p>Feedback comment: ${comment}</p>`,
+            `</div>`,
+        ].join('\n')
+    })
+
+    return res.status(201).json({ data: feedback });
 });
 
 app.listen(3333, () => {
